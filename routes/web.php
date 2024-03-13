9abd226391262f7b436268d424e6835adbab4147
Asier Osa <aosa.dev@gmail.com>
2024-03-13 22:31:11 +0100

    Actualizar para tener laravel-mix

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Define las rutas de recursos para los proyectos y las tareas.
+// Esto creará automáticamente todas las rutas necesarias para operaciones CRUD.
+Route::resource('proyectos', ProyectoController::class);
+Route::resource('tareas', TareaController::class);
+
+// Define la ruta principal ("/") de tu aplicación.
+// Cuando los usuarios accedan a la raíz de tu aplicación, verán la página de bienvenida.
 Route::get('/', function () {
     return view('welcome');
 });
